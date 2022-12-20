434e8f72d83633f0491f9f3adf4ff5f735f09fa7
minhlong149 <minhlong9514@gmail.com>
2022-12-20 13:53:00 +0700

    Config linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,21 @@
 module.exports = {
   env: {
-    browser: true,
+    commonjs: true,
     es2021: true,
+    node: true,
   },
-  extends: 'airbnb-base',
-  overrides: [],
+  extends: 'eslint:recommended',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
   },
   rules: {
-    'no-console': 'off',
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0,
   },
 };
--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { PORT } = process.env;
+let PORT = process.env.PORT;
 
 module.exports = {
   PORT,
