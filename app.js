21df4416edbd2d049bdd3a68c264bdb29e557f68
minhlong149 <minhlong9514@gmail.com>
2022-12-20 13:52:24 +0700

    Add logger middleware

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/blogs', blogsRouter);
+app.use(middleware.requestLogger);
+
+app.use('/api/notes', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
