fda67b1d2c989ae6366d28f8d87effd0d80c8a48
minhlong149 <minhlong9514@gmail.com>
2022-12-23 15:46:33 +0700

    Refactoring tests

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -14,57 +14,61 @@ beforeEach(async () => {
   });
 });
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-});
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs');
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
 
-  expect(response.body).toHaveLength(initialBlogs.length);
-});
+    expect(response.body).toHaveLength(initialBlogs.length);
+  });
 
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs');
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs');
 
-  const contents = response.body.map((blog) => blog.title);
-  expect(contents).toContain('Browser can execute only Javascript');
-});
+    const contents = response.body.map((blog) => blog.title);
+    expect(contents).toContain('Browser can execute only Javascript');
+  });
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'async/await simplifies making async calls',
-    author: 'Hoang Minh',
-    url: 'example.com',
-    likes: 20,
-  };
+  describe('addition of a new blog', () => {
+    test('a valid blog can be added', async () => {
+      const newBlog = {
+        title: 'async/await simplifies making async calls',
+        author: 'Hoang Minh',
+        url: 'example.com',
+        likes: 20,
+      };
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-  const blogsAtEnd = await blogsInDb();
+      const blogsAtEnd = await blogsInDb();
 
-  const contents = blogsAtEnd.map((blog) => blog.title);
+      const contents = blogsAtEnd.map((blog) => blog.title);
 
-  expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1);
+      expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1);
 
-  expect(contents).toContain('async/await simplifies making async calls');
-});
+      expect(contents).toContain('async/await simplifies making async calls');
+    });
 
-test('blog without content is not added', async () => {
-  const emptyBlog = {};
+    test('blog without content is not added', async () => {
+      const emptyBlog = {};
 
-  await api.post('/api/blogs').send(emptyBlog).expect(400);
+      await api.post('/api/blogs').send(emptyBlog).expect(400);
 
-  const blogsAtEnd = await blogsInDb();
+      const blogsAtEnd = await blogsInDb();
 
-  expect(blogsAtEnd).toHaveLength(initialBlogs.length);
+      expect(blogsAtEnd).toHaveLength(initialBlogs.length);
+    });
+  });
 });
 
 afterAll(() => {
