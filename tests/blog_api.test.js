506459448c2111da20d5c53f314ef75a54ee2eb1
minhlong149 <minhlong9514@gmail.com>
2022-12-23 13:54:54 +0700

    Initializing the database before tests

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,32 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog = require('../models/blog');
 
 const api = supertest(app);
+
+const initialBlogs = [
+  {
+    title: 'HTML is easy',
+    author: 'Long Nguyen',
+    url: 'example.com',
+    likes: 22,
+  },
+  {
+    title: 'Browser can execute only Javascript',
+    author: 'Hoang Minh',
+    url: 'example.com',
+    likes: 14,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  initialBlogs.forEach(async (blog) => {
+    const blogObject = new Blog(blog);
+    await blogObject.save();
+  });
+});
 
 test('blogs are returned as json', async () => {
   await api
@@ -11,16 +35,17 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/);
 }, 100000);
 
-test('there are two blogs', async () => {
+test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs');
 
-  expect(response.body).toHaveLength(2);
+  expect(response.body).toHaveLength(initialBlogs.length);
 });
 
-test('the first blog is Long\'s first post', async () => {
+test('a specific blog is within the returned blogs', async () => {
   const response = await api.get('/api/blogs');
 
-  expect(response.body[0].content).toBe('HTML is easy');
+  const contents = response.body.map((blog) => blog.title);
+  expect(contents).toContain('Browser can execute only Javascript');
 });
 
 afterAll(() => {
